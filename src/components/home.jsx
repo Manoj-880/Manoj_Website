4c78b5c3572d6f9a81628b8359b52a7a3d519c5a
Manoj-880 <manoj.inamanamelluri123@mail.com>
2023-09-17 08:50:52 +0530

    Added scroll to portfolio from home

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import HeroImage from '../assets/my_picture.jpg';
 import {MdOutlineKeyboardArrowRight} from 'react-icons/md';
+import { Link } from 'react-scroll';
 
 const Home = () => {
   return (
@@ -10,12 +11,12 @@ const Home = () => {
                 <h2 className='text-4xl sm:text-7xl font-bold text-white'>I'm a UI/UX Designer</h2>
                 <p className='text-gray-500 py-4 max-w-md'>I have 1 year of experience building and designing software. Currently, I love to work on web application using technologies like React, Tailwind, Node and MongoDb.</p>
                 <div>
-                    <button className='group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer'>
+                    <Link to='portfolio' smooth duration={500} className='group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer'>
                         Portfolio
                         <span className='group-hover:rotate-90 duration-300'>
                            <MdOutlineKeyboardArrowRight size={25} className='ml-1'/> 
                         </span>
-                    </button>
+                    </Link>
                 </div>
             </div>
             <div>
