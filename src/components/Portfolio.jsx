cde81e825334e551662911074822d497e9ba3487
Manoj-880 <manoj.inamanamelluri123@mail.com>
2023-09-17 08:43:27 +0530

    Added links for design and caseStusy

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -10,20 +10,30 @@ const Portfolio = () => {
 const projects = [
     {
         id:1,
-        src: jagbandhu
+        src: jagbandhu,
+        designLink: "https://www.figma.com/file/FXYuo4FFk4ymCOELxQNLn4/jagbandhu-redesign?type=design&node-id=0%3A1&mode=design&t=DF1jW9KKXtygDoIi-1",
+        caseStudy:"https://www.behance.net/gallery/179980253/JagBandhu",
     },
     {
         id:2,
-        src: nehwe
+        src: nehwe,
+        designLink: "https://www.figma.com/file/wtrRjPEirY1gJt825hJGZv/Nehwe?type=design&node-id=0%3A1&mode=design&t=0VwnU8A1tith3JAn-1",
+        caseStudy:"https://www.behance.net/gallery/179979811/Nehwe-Elearning-Platform",
     },{
         id:3,
-        src: fcf
+        src: fcf,
+        designLink: "https://www.figma.com/file/oJSEDkDYxIMC9KTqY9W8Ks/web-application?type=design&node-id=0%3A1&mode=design&t=RK7KgrANqSs7ZcoT-1",
+        caseStudy:"https://www.behance.net/gallery/157770849/Feed-Care-Fear-web-application",
     },{
         id:4,
-        src: task
+        src: task,
+        designLink: "https://www.figma.com/file/dCzKk4v7xrg6YK34Fwg4r6/Task-reminder-application-ios?type=design&node-id=0%3A1&mode=design&t=11JD8fYsTo67EMxd-1",
+        caseStudy:"https://www.behance.net/gallery/147575645/Task-Reminder-application",
     },{
         id:5,
-        src: hourstay
+        src: hourstay,
+        designLink: "https://www.figma.com/file/cA3RpmIhotpgWxiuGuSEhf/Hour-Stay?type=design&node-id=0%3A1&mode=design&t=mr1HxZfeShUewCCv-1",
+        caseStudy:"https://www.behance.net/gallery/143142399/Hotel-Booking-mobile-application",
     },
 ]
 
@@ -36,12 +46,12 @@ const projects = [
             </div>            
             <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0'>
                 {
-                    projects.map(({id, src}) => (
+                    projects.map(({id, src, designLink, caseStudy}) => (
                         <div key={id} className='shadow-md shadow-gray-600 rounded-lg'>
                             <img src={src} alt="" className='rounded-md duration-200 hover:scale-105' />
                             <div className='flex items-center justify-center'>
-                                <button className='w-2/5 px-6 py-3 m-4 duration-200 hover:scale-105'>Design</button>
-                                <button className='w-2/5 px-4 py-3 m-4 duration-200 hover:scale-105'>Case Study</button>
+                                <a href={designLink} target='_blank' rel='noreferrer' className='w-2/5 px-6 py-3 m-4 duration-200 hover:scale-105'>Design</a>
+                                <a href={caseStudy} target='_blank' rel='noreferrer' className='w-2/5 px-4 py-3 m-4 duration-200 hover:scale-105'>Case Study</a>
                             </div>
                         </div>
                     ))
